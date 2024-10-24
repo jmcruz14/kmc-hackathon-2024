f558d3f4f98072dc8eb42e781ad27658347cde10
Jay <jccruz009@yahoo.com>
2024-10-25 02:13:23 +0800

    feat: update routing

--- frontend/src/pages/MainPortal.vue
+++ frontend/src/pages/MainPortal.vue
@@ -1,13 +1,27 @@
 <template>
   <Ribbon></Ribbon>
-  <NavBar>
-    <span class="text-3xl">Job Application Profile</span>
+
+  <!-- CONTENT HERE -->
+
+  <NavBar v-if="type === 'applicant'">
+    <span class="text-xl">Applications</span>
+    <span class="text-xl">Openings</span>
+    <span class="text-xl">Schedule</span>
+    <span class="text-xl">Profile</span>
+  </NavBar>
+  <NavBar v-if="type === 'employer'">
+    <span class="text-xl">Dashboard</span>
+    <span class="text-xl">Applications</span>
+    <span class="text-xl">Schedule</span>
+    <!-- <span class="text-xl">Job Application Profile</span> -->
   </NavBar>
 </template>
 
 <script>
 import Ribbon from '@/components/Ribbon.vue';
 import NavBar from '@/components/NavBar.vue';
+import { useRoute } from 'vue-router';
+import { ref, computed } from 'vue';
 
 export default {
   components: {
@@ -15,7 +29,13 @@ export default {
     NavBar
   },
   setup () {
-    return {}
+    const route = useRoute()
+    const type = computed(() => route?.query?.type)
+    console.warn(route)
+
+    return {
+      type
+    }
   }
 }
 
